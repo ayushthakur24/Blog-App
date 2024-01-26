f4292a9abf02c4049713ae8b9e9e27f32eb1e2c1
Ayush Thakur <AT382377@gmail.com>
2024-01-26 16:09:40 -0700

    Added Login and Registration endpoints.

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -3,25 +3,40 @@ package com.springboot.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    //Authentication Manager uses userDetailsService to get the details from db, also uses password encoder for
+    // encryption and decryption
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf()
@@ -31,12 +46,17 @@ public class SecurityConfig {
                         //authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
 
                         //Below code only allows GET method to be accessed by all.
-                authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll().anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+                        authorize.
+                        requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                                //whitelisted login auth api
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
+    //In-memory db authentication
     //To fetch specific usernames with roles and password
-    @Bean
+   /* @Bean
     public UserDetailsService userDetailsService(){
         UserDetails ayush = User.builder()
                 .username("ayush")
@@ -51,5 +71,5 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(ayush,admin);
-    }
+    }*/
 }
